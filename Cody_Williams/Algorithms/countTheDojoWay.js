fefa089edec573ec57ca666aa22abdb039a5beab
codyw9524 <codyw9524@gmail.com>
2017-01-26 10:40:30 -0600

    added algo solutions for 1/26/17

---
+++ Cody_Williams/Algorithms/countTheDojoWay.js
@@ -0,0 +1,15 @@
+function countTheDojoWay(){
+	for(var i = 1; i < 101; i++){
+		if(i % 10 == 0){
+			console.log('Coding Dojo');
+		}
+		else if(i % 5 == 0){
+			console.log('Coding');
+		}
+		else{
+			console.log(i);
+		}
+	}
+}
+
+countTheDojoWay();
---
+++ Cody_Williams/Algorithms/multOfThree.js
@@ -0,0 +1,12 @@
+function multOfThree(){
+	for(var i = -300; i < 0; i+=3){
+		if(i == -3 || i == -6){
+			continue;
+		}
+		else {
+			console.log(i);
+		}
+	}
+}
+
+multOfThree();
